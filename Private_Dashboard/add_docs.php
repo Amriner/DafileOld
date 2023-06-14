f095b7372c8fbe20f66b51ab731e8e6cfa3f0b75
Amrin Hafiz <17205136@siswa.um.edu.my>
2023-06-14 11:39:49 +0800

    Main insert

---
+++ Private_Dashboard/add_docs.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Edit Document</title>
+    <meta name="description" content="Roughly 155 characters">
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <div class="container">
+        <form action="#" method="POST">
+        <div class="title">
+            Add Document
+        </div>
+
+        <div class="form">
+            <div class="input_field">
+                <label>Document Name</label>
+                <input type="text" class="input" name="dname">
+            </div>
+
+            <div class="input_field">
+                <label>A/C Type</label>
+                <input type="text" class="input" name="actype">
+            </div>
+
+            <div class="input_field">
+                <label>A/C Registration</label>
+                <input type="text" class="input" name="acregn">
+            </div>
+
+            <div class="input_field">
+                <label>Document Date</label>
+                <input type="text" class="input" name="docdate">
+            </div>
+
+            <div class="input_field">
+                <input type="submit" value="AddDocs" class="btn" name="add_docs>
+            </div>
+        </div>
+    </form>
+    </div>
+                
+
+  </body>
+</html>
